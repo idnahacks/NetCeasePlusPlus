6f1db999ec5fca5292f1cc028cadecd1d42f9933
idna <12153311+idnahacks@users.noreply.github.com>
2022-01-06 17:48:05 +0000

    Add new modules and fix some bugs

--- netcease++.ps1
+++ netcease++.ps1
@@ -44,13 +44,44 @@ function Get-SessionEnumPermissions {
         [switch]
         $fromreg
     )
-    if ($fromreg) {
-        Get-SessRegkey
-    }
-    else {
+    if ($rawdata) {
+        
         Get-SessRawvalue -rawdata $rawdata
     }
+    else {
+        Get-SessRegkey
+    }
     $csd | select -ExpandProperty DiscretionaryAcl | ForEach-Object { $_ | Add-Member -MemberType ScriptProperty -Name TranslatedSID -Value ({$this.SecurityIdentifier.Translate([System.Security.Principal.NTAccount]).Value}) -PassThru}
+}
+
+function Set-DefaultNetCeasePermissions {
+    <#
+    .DESCRIPTION
+        Sets the default hardened Session Enumeration permissions as set in the original NetCease script and either outputs the hex string to set by GPO, or sets directly in the local registry.
+    #>
+    param (
+        [Parameter(HelpMessage = "Sets the new permissions in the local registry (requires Powershell to be running as Administrator)")]
+        [switch]
+        $toreg
+    )
+    Get-SessRawvalue -rawdata 010004801400000020000000000000002c00000001010000000000051200000001010000000000051200000002008c000600000000001400ff011f0001010000000000050300000000001400ff011f0001010000000000050400000000001400ff011f000101000000000005060000000000180013000f00010200000000000520000000200200000000180013000f00010200000000000520000000230200000000180013000f0001020000000000052000000025020000
+    $SRVSVC_SESSION_USER_INFO_GET = 0x00000001
+    $data = New-Object -TypeName System.Byte[] -ArgumentList $csd.BinaryLength
+    $csd.GetBinaryForm($data,0)
+    if ($toreg) {
+        $key = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\DefaultSecurity"
+        $name = "SrvsvcSessionInfo"
+        Set-ItemProperty -Path $key -Name $name -Value $data
+    }
+    else {
+        $hexarray = @()
+        foreach ($d in $data) {
+        $h = '{0:x2}' -f $d
+        $hexarray += $h
+        }
+        $hexstring = $hexarray -join ''
+        $hexstring
+    }
 }
 
 function Add-SessionEnumUser {
@@ -72,12 +103,22 @@ function Add-SessionEnumUser {
         [string]
         $rawdata
     )
-    if ($fromreg) {
-        Get-SessRegkey
-    }
-    else {
-        Get-SessRawvalue -rawdata $rawdata
-    }
+        if ($fromreg) {
+            Get-SessRegkey -ErrorAction STOP
+        }
+        else {
+            try {
+                Get-SessRawvalue -rawdata $rawdata -ErrorAction STOP
+                }
+            catch [System.Management.Automation.ParameterBindingException]{
+                Write-Error "Either rawdata or fromreg must be selected as the input" -ErrorAction STOP
+            }
+            catch {
+                Write-Error $PSItem.Exception.Message -ErrorAction STOP
+            }
+        }
+    
+
     $SRVSVC_SESSION_USER_INFO_GET = 0x00000001
     $ntuser = New-Object System.Security.Principal.NTAccount($user)
     $sid = $ntuser.Translate([System.Security.Principal.SecurityIdentifier])
@@ -119,6 +160,20 @@ function Remove-SessionEnumUser {
         [string]
         $rawdata
     )
+    if ($fromreg) {
+        Get-SessRegkey -ErrorAction STOP
+    }
+    else {
+        try {
+            Get-SessRawvalue -rawdata $rawdata -ErrorAction STOP
+            }
+        catch [System.Management.Automation.ParameterBindingException]{
+            Write-Error "Either rawdata or fromreg must be selected as the input" -ErrorAction STOP
+        }
+        catch {
+            Write-Error $PSItem.Exception.Message -ErrorAction STOP
+        }
+    }
     $SRVSVC_SESSION_USER_INFO_GET = 0x00000001
     $ntuser = New-Object System.Security.Principal.NTAccount($user)
     $sid = $ntuser.Translate([System.Security.Principal.SecurityIdentifier])
@@ -152,10 +207,21 @@ function Get-RemoteSAMPermissions {
     )
     if ($fromreg) {
         Get-SamRegKey
+        Write-Host "`n`nYour local RemoteSAM permissions in the local registry are:`n"
         ConvertFrom-SddlString -sddl $restrictremotesam | select -ExpandProperty DiscretionaryAcl
+        Write-Host "`n`nThis translates to an SDDL of:`n"
+        $restrictremotesam
     }
     else {
-        ConvertFrom-SddlString -sddl $rawdata | select -ExpandProperty DiscretionaryAcl
+        try{
+            ConvertFrom-SddlString -sddl $rawdata | select -ExpandProperty DiscretionaryAcl
+        }
+        catch [System.Management.Automation.ParameterBindingException]{
+            Write-Error "Either rawdata or fromreg must be selected as the input" -ErrorAction STOP
+        }
+        catch {
+            Write-Error $PSItem.Exception.Message -ErrorAction STOP
+        }
     }
 }
 
@@ -212,8 +278,57 @@ function Add-RemoteSAMUser {
         Set-ItemProperty -Path $key -Name $name -Value $newsddl
     }
     else {
-        Write-Host "`n`nYour new SDDL is `n$($newsddl)`n`n"
+        Write-Host "`n`nYour new SDDL to be deployed by GPO is`n$($newsddl)`n`n"
         Write-Host "This translates to the following ACL:`n"
         ConvertFrom-SddlString -sddl $newsddl | select -ExpandProperty DiscretionaryAcl
+    }
+}
+
+function Remove-RemoteSAMUser {
+    <#
+    .DESCRIPTION
+        Removes a user or group from the Remote SAM Enumeration Permissions and either outputs the SDDL string to set by GPO, or sets directly in the local registry.
+    #>
+    param (
+        [Parameter(Mandatory, HelpMessage = "The user or group to remove, in the format domain\user or domain\group")]
+        [string]
+        $user,
+        [Parameter(HelpMessage = "Sets the new permissions in the local registry (requires Powershell to be running as Administrator)")]
+        [switch]
+        $toreg,
+        [Parameter(HelpMessage = "Reads the settings from the local registry")]
+        [switch]
+        $fromreg,
+        [Parameter(HelpMessage = "Enter the raw SDDL string from the GPO GUI")]
+        [string]
+        $rawdata
+    )
+    $remoteAccess = 0x00020000
+    $ntuser = New-Object System.Security.Principal.NTAccount($user)
+    $sid = $ntuser.Translate([System.Security.Principal.SecurityIdentifier])
+    if ($fromreg) {
+        Get-SamRegKey
+        $rsd = New-Object -TypeName System.Security.AccessControl.RawSecurityDescriptor -ArgumentList $restrictremotesam
+    }
+    else {
+        $rsd = New-Object -TypeName System.Security.AccessControl.RawSecurityDescriptor -ArgumentList $rawdata
+    }
+    if (($sid.Value -in $rsd.DiscretionaryAcl.SecurityIdentifier.value) -eq $true) {
+        $aclindex = [array]::indexof($rsd.DiscretionaryAcl.securityidentifier.value,$sid.Value)
+        $rsd.DiscretionaryAcl.RemoveAce($aclindex)
+        $newsddl = $rsd.GetSddlForm([System.Security.AccessControl.AccessControlSections]::All)
+        if ($toreg) {
+            $key = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+            $name = "RestrictRemoteSAM"
+            Set-ItemProperty -Path $key -Name $name -Value $newsddl
+        }
+        else {
+            Write-Host "`n`nYour new SDDL to be deployed by GPO is`n$($newsddl)`n`n"
+            Write-Host "This translates to the following ACL:`n"
+            ConvertFrom-SddlString -sddl $newsddl | select -ExpandProperty DiscretionaryAcl
+        }
+    }
+    else {
+        Write-Host "`n$($user) does not have RemoteSAM permissions.`n"
     }
 }
